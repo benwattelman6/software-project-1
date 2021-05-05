061f2fbb977f21999a57c2a5e3f1b1cb45209f74
Ben Wattelman <benwattelman6@gmail.com>
2021-05-05 11:05:12 +0300

    Finished main, left important TODOs

--- kmeans.c
+++ kmeans.c
@@ -1,13 +1,128 @@
 #include <stdio.h>;
 #include <stdlib.h>;
 #include <assert.h>;
-#include "kmeans.h"
+#include <kmeans.h>; //TODO: why is this here? Commenting out derives several errors in code
 
 
 typedef enum HasChangedEnum {
     TRUE = 1,
     FALSE = 0
 } HasChanged;
+
+
+/*** Function Declaration ***/
+
+int get_num_points(FILE *fptr);
+int get_num_coordinates(FILE *fptr);
+void get_points_from_file (int numPoints, int numCoordinates, FILE *fptr, double *points);
+void reset_array_double(double * arr, int size);
+void reset_array_int(int * arr, int size);
+double* split_points_to_clusters(double* centroids, double* points, int point_size, int k, int points_num);
+HasChanged calculate_new_centroids(double *centroids, double *new_centroids, int* centroids_count, int point_size, int k);
+double get_distance (double *points, int startIndex1, int startIndex2, int numCoordinates);
+
+
+/*** Main Function ***/
+
+int main(int argc, char **argv) { 
+    //argc = number of parameters (including program name), **argv = string array of parameters. Reference: https://stackoverflow.com/questions/29584898/c-getting-input-from-cmd
+    //TODO: make sure how input is actually formatted
+    assert (argc == 3 || argc = 4);
+    int k, max_iter;
+    char *filename;
+    int numPoints, numCoordinates;
+
+    if (argc == 3) { //max_iter not passed as argument
+        assert (atoi(argv[0] > 0));
+        k = atoi(argv[0]);
+        max_iter = 200;
+        filename = argv[2];
+    }
+    else { //argc == 4
+        assert (atoi(argv[1] > 0 && atoi(argv[2] > 0)); 
+        k = atoi(argv[0]);
+        max_iter = atoi(argv[1]);
+        filename = argv[4];
+    }
+
+    FILE *fptr;
+    fptr = fopen(filename,"r");
+    if (fptr != NULL) {
+        numPoints = get_num_points(fptr);
+        numCoordinates = get_num_coordinates(fptr);
+    }
+
+    double *points = malloc(sizeof *points * numPoints * numCoordinates); //TODO: probably not the best way to allocate memory for 2D-array. Reference: https://stackoverflow.com/questions/9381711/how-to-declare-a-variable-size-2d-array-in-c
+    get_points_from_file(numPoints, numCoordinates, fptr, points);
+
+    fclose(fptr);
+
+    // initiate the centroids to be the first k points
+    double *centroids = malloc(sizeof *centroids k * numPoints);
+    for (int i=0; i<k; i++) {
+        for (int j=0; j< numCoordinates; j++) {
+            centroids[i*numCoordinates + j] = points[i*numCoordinates + j] 
+        }
+    }
+
+    while (max_iter > 0) {
+        split_points_to_clusters (centroids, points, numCoordinates, k, numPoints);
+        if (calculate_new_centroids(centroids, , , k == FALSE)) { //TODO: what should "new_centroids" and "controid_count" parameters be? 
+            max_iter = 0; //breaks the loop
+        } 
+        max_iter -= 1;
+    }
+
+    //print final centroids
+    for (int i=0; i<k; i++) {
+        int j = 0;
+        while (j < numCoordinates -1) {
+            printf ("%lf," , centroids[i*numCoordinates + j]);
+            j++;
+        }
+        printf ("%lf/n" , centroids[i*numCoordinates + j]) //print last coordinate
+    }
+
+    return 0;
+
+}
+
+
+/*** Function Implementation ***/
+
+int get_num_points(FILE *fptr) {
+    int countPoints = 1;
+    char ch;
+    while (ch = fgetc(fptr) != EOF) {
+        if (ch == '\n') {
+            countPoints++;
+        }
+    }
+    rewind(fptr);
+    return countPoints;
+}
+
+int get_num_coordinates(FILE *fptr) {
+    int countCoordinates = 1;
+    char ch;
+    while (ch = fgetc(fptr) != '\n') {
+        if (ch == ',') {
+            countCoordinates++;
+        }
+    }
+    rewind(fptr);
+    return countCoordinates;
+}
+
+void get_points_from_file (int numPoints, int numCoordinates, FILE *fptr, double *points) {
+    for (int pointIndex=0; pointIndex<numPoints; pointIndex++) {
+        for (int coordinateIndex=0; coordinateIndex<numCoordinates; coordinateIndex++) {
+            fscanf(fptr , "%lf,\n", points[pointIndex * numCoordinates + coordinateIndex]); //Reference: https://stackoverflow.com/questions/43286609/how-to-use-fscanf-to-read-doubles-in-c 
+            //TODO: make sure this fscanf format actually catches all of our doubles
+        }
+    }
+    
+}
 
 
 void reset_array_double(double * arr, int size) {
@@ -44,6 +159,8 @@ double* split_points_to_clusters(double* centroids, double* points, int point_si
         }
         *(clusters_count + min_centroid) ++; // increase counter
     }
+
+    //TODO: what about return value?
 }
 
 HasChanged calculate_new_centroids(double *centroids, double *new_centroids, int* centroids_count, int point_size, int k) {
@@ -60,62 +177,8 @@ HasChanged calculate_new_centroids(double *centroids, double *new_centroids, int
 }
 
 
-int getNumPoints(FILE *fptr) {
-    int countPoints = 1;
-    char ch;
-    while (ch = fgetc(fptr) != EOF) {
-        if (ch == '\n') {
-            countPoints++;
-        }
-    }
-    rewind(fptr);
-    return countPoints;
-}
 
-int getNumCoordinates(FILE *fptr) {
-    int countCoordinates = 1;
-    char ch;
-    while (ch = fgetc(fptr) != '\n') {
-        if (ch == ',') {
-            countCoordinates++;
-        }
-    }
-    rewind(fptr);
-    return countCoordinates;
-}
-
-void getPointsFromFile (int numPoints, int numCoordinates, FILE *fptr, double *points) {
-    for (int pointIndex=0; pointIndex<numPoints; pointIndex++) {
-        for (int coordinateIndex=0; coordinateIndex<numCoordinates; coordinateIndex++) {
-            fscanf(fptr , "%lf,\n", points[pointIndex * numPoints]); //Reference: https://stackoverflow.com/questions/43286609/how-to-use-fscanf-to-read-doubles-in-c 
-            //TODO: make sure this fscanf format actually catches all of our doubles
-        }
-    }
-    
-}
-
-
-
-double *get_points (char *filename) {
-    FILE *fptr;
-    fptr = fopen(filename,"r");
-    if (fptr != NULL) {
-        int numPoints = getNumPoints(fptr);
-        int numCoordinates = getNumCoordinates(fptr);
-
-    //Allocate memory for 2D-array
-    double *points = malloc(sizeof *points * numPoints * numCoordinates); //TODO: probably not the best way to allocate memory for 2D-array. Reference: https://stackoverflow.com/questions/9381711/how-to-declare-a-variable-size-2d-array-in-c
-    
-    //Fill array with parsed values from files
-    getPointsFromFile(numPoints , numCoordinates , fptr , points);
-
-    }
-
-    fclose(fptr);
-    
-}
-
-double getDistance (double *points, int startIndex1, int startIndex2, int numCoordinates) { //TODO: make sure we send  numCoordinates as parameters in main
+double get_distance (double *points, int startIndex1, int startIndex2, int numCoordinates) { //TODO: adjust parameter to the function use in split_points_to_clusters
     double distance = 0;
     for (int i=0; i<numCoordinates; i++) {
         distance += pow(points[startIndex1 + i] - points[startIndex2 + i], 2);
@@ -123,35 +186,4 @@ double getDistance (double *points, int startIndex1, int startIndex2, int numCoo
     return distance;
 }
 
-int main(int argc, char **argv) { //argc = number of parameters (including program name), **argv = string array of parameters. Reference: https://stackoverflow.com/questions/29584898/c-getting-input-from-cmd
-    //TODO: make sure how input is actually formatted
-    assert (argc == 3 || argc = 4);
-    int k, max_iter;
-    char *filename;
-    if (argc == 3) { //max_iter not passed as argument
-        assert (atoi(argv[0] > 0));
-        k = atoi(argv[0]);
-        max_iter = 200;
-        filename = argv[2];
-    }
-    else { //argc == 4
-        assert (atoi(argv[1] > 0 && atoi(argv[2] > 0)); 
-        k = atoi(argv[0]);
-        max_iter = atoi(argv[1]);
-        filename = argv[4];
-    }
 
-    //TODO: refactor getNumPoints & getNumCoordinates so they are called from main, FILE should also be created in main
-    int numPoints = getNumPoints();
-    int numCoordinates = getNumCoordinates();
-
-    double *points = get_points(filename);
-    double **centroids = malloc(k*numPoints);
-    for (int i=0; i<k; i++) {
-        
-    }
-
-
-    double *points = get_points("input_1.txt");
-    printf("points[%d]" , 0);
-}
